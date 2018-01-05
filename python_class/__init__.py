90f28443dfe8072759000636a429a84e9010d425
Fadhiil Rachman <fadhiilrachman@gmail.com>
2018-01-05 14:55:01 +0700

    Update __init__.py

--- python_class/__init__.py
+++ python_class/__init__.py
@@ -3,7 +3,7 @@ from .pohonpakar import PohonPakar
 
 __copyright__       = 'Copyright 2018 by Fadhiil Rachman'
 __version__         = '1.0.0'
-__license__         = 'MIT'
+__license__         = 'GPL-3.0'
 __author__          = 'Fadhiil Rachman'
 __author_email__    = 'fadhiilrachman@gmail.com'
 __url__             = 'https://github.com/fadhiilrachman/goldar-js'
